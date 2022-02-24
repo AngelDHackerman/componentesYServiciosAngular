cfd173499c9d40fdc0396dda41eaee28e068fe20
angelDariaux <angeldariaux@gmail.com>
2022-02-23 22:35:38 -0600

    vimos el tiempo de vida de los componentes

--- my-store/src/app/components/img/img.component.ts
+++ my-store/src/app/components/img/img.component.ts
@@ -1,19 +1,43 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'; // ! Input para recibir del modulo padre, Output para enviar al modulo padre, EventEmitter para detectar eventos
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy } from '@angular/core'; // ! Input para recibir del modulo padre, Output para enviar al modulo padre, EventEmitter para detectar eventos
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
   @Input() img: string = ''; // ? @Input() Asi podemos recibir info desde el componente padre
   @Output() loaded = new EventEmitter <string> ();
   imageDefault = '../../../assets/notWorking.png';
 
-  constructor() { }
+  constructor() {
+    // ? Corre antes del render
+    // ? NO correcr cosas async -- one time
+    console.log('constructor', 'imgValue =>', this.img)
+   }
+
+   ngOnChanges() {
+    // ? Corre antes y durante el render
+    // ? Mantiene actualizados los cambios en los inputs -- several times.
+    console.log('ngOnchanges', 'imgValue => ', this.img)
+   }
 
   ngOnInit (): void {
+    // ? Corre antes del render
+    // ? Podemos correcr cosas async - como los fetch -- one time
+    console.log('ngOnInit', 'imgValue => ', this.img)
+  }
+
+  ngAfterViewInit() {
+    // ? Corre despues del render
+    // ? Maneja los hijos
+    console.log('ngAfterViewInit')
+  }
+
+  ngOnDestroy() {
+    //  ? corre cuando se elimina un componente
+    console.log('ngOnDestroy');
   }
 
   imgError () {
