7217d4f26451815dea012a9c1ea2c479947313a3
angelDariaux <angeldariaux@gmail.com>
2022-04-24 17:25:49 -0600

    creando el segundo componente de nuestra web app

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+
+import { Product } from './models/product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,6 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = '';   // https://http.cat/599.jpg
+
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'EL mejor juguete',
+      price: 565,
+      image: 'http://placekitten.com/200/300'
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: 'http://placekitten.com/200/300'
+    },
+    {
+      id: '3',
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: 'http://placekitten.com/200/300'
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      price: 23,
+      image: 'http://placekitten.com/200/300'
+    },
+  ]
 
   onLoaded(img: string) {
     console.log('log padre', img);
